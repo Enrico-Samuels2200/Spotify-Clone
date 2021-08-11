7443f8de9c55e0ca010b548ad08055ce41869d56
Enrico-Samuels2200 <enricosamuels00@gmail.com>
2021-08-11 09:02:58 +0200

    Added login and signup feature to
    "user_auth.js".

--- models/user_model.js
+++ models/user_model.js
@@ -7,8 +7,17 @@ const sign_up_schema = mongoose.Schema({
     password: String
 })
 
-const new_user = mongoose.model("Account", sign_up_schema, "user_accounts");
+const login_schema = mongoose.Schema({
+    email: String,
+    username: String,
+    password: String
+})
+
+const user_account = mongoose.model("Account", sign_up_schema, "user_accounts");
+const login_account = mongoose.model("Login_Account", login_schema, "user_accounts");
+
 
 module.export = {
-    new_user
+    user_account,
+    login_account
 }
--- routes/user_auth.js
+++ routes/user_auth.js
@@ -1,13 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // Request mongoose models
-const { new_user } = require('../models/user_model');
+const { user_account, login_account } = require('../models/user_model');
 
-router.post('/sign-up', (req, res) => {
+router.post('/sign-up', async (req, res) => {
 
-    res.send()
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashed_password = await bcrypt.hash(req.body.password, salt)
+
+    const email_exist = await user_account.findOne({ email: req.body.email });
+    const user_exist = await user_account.findOne({ username: req.body.username });
+
+    if (email_exist) return res.status(400).send("Email already exist.");
+    if (user_exist) return res.status(400).send("Username already taken");
+
+    // Create new user
+    const client_data = new user_account({
+        email: req.body.email,
+        name: req.body.usernname,
+        password: hashed_password
+    });
+
+    // Returns a promise
+    try {
+        await client_data.save();
+        res.send("Account successfully created.");
+    }
+    catch (err) {
+        res.json({ message: err });
+    };
 });
 
+
+router.post('/login', async (req, res) => {
+
+    const client_data = new login_account({
+        _id: user_id,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    const user = await login_account.findOne({ email: req.body.email });
+
+    //const validPassword = await bcrypt.compare(req.body.password, user.password);
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+    // If the users email is not found then return error
+    if (!user) return res.status(400).send("Invalid email or password entered.");
+    if (!validPassword) return res.status(400).send('Invalid email or password entered.')
+
+    // Sends the user a web roken to identify that he/she is logged in
+
+    const token = jwt.sign({ _id: client_data._id }, process.env.token_secret);
+    res.header('auth-token', token).send(token);
+});
 
 module.exports = router;
